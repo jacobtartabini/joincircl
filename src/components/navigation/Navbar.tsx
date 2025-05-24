4d71e8a08d803e4c5c9871618b47d541c4d60965
jacobtartabini <139934788+jacobtartabini@users.noreply.github.com>
2025-05-24 15:02:03 -0400

    Update Navbar.tsx

--- src/components/navigation/Navbar.tsx
+++ src/components/navigation/Navbar.tsx
@@ -161,7 +161,7 @@ export function Navbar() {
       {/* Add New Person Button */}
       <Button className="rounded-xl">
         <Plus className="h-4 w-4 mr-2" />
-        New Contact
+        Add Contact
       </Button>
       
       {/* New Tag Dialog */}
